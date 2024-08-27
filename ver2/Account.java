b5a295aa16ca8d35eb61b48c0fe68a65257ca2a5
Elayna Breanne Raby <ebraby@valdosta.edu>
2024-08-27 01:32:47 -0400

    Submission of lab 3 files

---
+++ ver2/Account.java
@@ -0,0 +1,79 @@
+package ver2;
+
+public class Account {
+
+	// Instance variable, only available inside this class.
+
+	private double balance;
+	
+
+	// Constructor
+
+	public Account(double initBalance) {
+
+	balance = initBalance;
+	
+	
+
+	}
+	
+	
+
+	Account a1 = new Account(1000.0);
+
+	System.out.println("Balance=$" + a1.getBalance());
+
+	a1.deposit(500.0);
+
+	System.out.println("Balance=$" + a1.getBalance());
+	
+	// Decreases the balance by amount
+
+		public void withdraw(double amount) {
+
+		if(amount>0) {
+
+		balance -= amount;
+		
+		a1.withdraw(200.0);
+
+		System.out.println("Balance=$" + a1.getBalance());
+
+		}
+		@Override
+
+		public String toString() {
+
+		String msg = "balance=$" + balance;
+
+		return msg;
+		
+		System.out.println(a1.toString());
+
+		System.out.println(a1);
+
+		}
+		public static void main(String[] args) {
+
+			Account a1 = new Account(1000.0);
+
+			System.out.println("Balance=$" + a1.getBalance());
+
+			a1.deposit(500.0);
+
+			System.out.println("Balance=$" + a1.getBalance());
+
+			a1.withdraw(200.0);
+
+			System.out.println("Balance=$" + a1.getBalance());
+
+			System.out.println(a1.toString());
+
+			System.out.println(a1);
+
+			}
+	
+	}
+		
+
+	}
